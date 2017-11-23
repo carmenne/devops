044738a1c1d9cd7371a50ee2b26d3f848e79fdfa
= <=>
2017-11-23 13:59:08 +0100

    Add login functionality (remaining

--- src/main/java/com/carmenne/web/controllers/LoginController.java
+++ src/main/java/com/carmenne/web/controllers/LoginController.java
@@ -1,7 +1,15 @@
 package com.carmenne.web.controllers;
 
-/**
- * Created by carmen on 23-11-17.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
+
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login() {
+        return LOGIN_VIEW_NAME;
+    }
 }
